ef708de808d493b6f7b5fdedef09410d5c3d7237
Guilherme Medeiros <gsmedeiros3@ucs.br>
2025-04-12 15:06:44 -0300

    Backend

---
+++ backend/src/main/java/com/saveandeat/SaveandeatApplication.java
@@ -0,0 +1,13 @@
+package com.saveandeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveandeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveandeatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/saveandeat/SaveandeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveandeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveandeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
