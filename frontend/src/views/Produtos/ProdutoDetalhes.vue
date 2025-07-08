64aeef303a57b39fd73e07445339128e8ce709b3
Guilherme Medeiros <guilherme.medeiros@keyworks.com.br>
2025-07-08 16:42:24 -0300

    Detalhes produto finalizado

--- frontend/src/views/Produtos/ProdutoDetalhes.vue
+++ frontend/src/views/Produtos/ProdutoDetalhes.vue
@@ -1,9 +1,19 @@
 <template>
-    <v-container>
-        <NavigationDrawer>
-            <h1>Mais Detalhes do Produto</h1>
-            <div class="produto-detalhes" v-if="produto">
+    <NavigationDrawer>
+        <v-container class="tela-produto-detalhes">
+            <v-row class="header-linha">
+                <v-col cols="9">
+                    <h1 class="titulo-pagina">Mais Detalhes do Produto</h1>
+                </v-col>
+                <v-col cols="3" class="text-right">
+                    <v-btn class="bt-voltar" @click="$router.go(-1)">
+                        Voltar
+                    </v-btn>
+                </v-col>
+            </v-row>
 
+            <!-- Caixa com fundo branco -->
+            <div class="produto-detalhes" v-if="produto">
                 <h1>{{ produto.nome }}</h1>
                 <p>{{ produto.descricao }}</p>
                 <p>{{ produto.quantidade }} produtos em estoque</p>
@@ -20,25 +30,20 @@
                     <v-col cols="6">
                         <BaseInput id="precoOriginal" v-model="produto.precoOriginal" label="Preço Original"
                             :readonly="true" />
-
                     </v-col>
                     <v-col cols="6">
                         <BaseInput id="precoDesconto" v-model="produto.precoDesconto" label="Preço com Desconto"
                             :readonly="true" />
-
                     </v-col>
                 </v-row>
                 <BaseInput id="categoria" v-model="produto.categoria" label="Categoria" :readonly="true" />
                 <v-row>
                     <v-col cols="6">
                         <BaseInput id="empresa" v-model="empresa.nome" label="Empresa Responsável" :readonly="true" />
-
-
                     </v-col>
                     <v-col cols="6">
                         <BaseInput id="logradouro" v-model="empresa.localizacao.logradouro" label="Logradouro"
                             :readonly="true" />
-
                     </v-col>
                 </v-row>
                 <v-row>
@@ -51,18 +56,21 @@
                 </v-row>
                 <BaseInput id="cidade" v-model="empresa.localizacao.cidade" label="Cidade" :readonly="true" />
             </div>
-        </NavigationDrawer>
-    </v-container>
+        </v-container>
+    </NavigationDrawer>
 </template>
 
 <script lang="ts">
 import NavigationDrawer from '@/components/NavigationDrawer.vue'
 import BaseInput from '@/components/BaseInput.vue'
 import { buscarPorId } from '@/services/produtoService'
+import { buscarPorIdEmpresa } from '@/services/empresaService'
 import type { Produto } from '@/interface/produto'
 import type { Empresa } from '@/interface/empresa'
-import { buscarPorIdEmpresa } from '@/services/empresaService'
-export default {
+import { Route } from 'vue-router'
+import Vue from 'vue'
+
+export default Vue.extend({
     name: 'ProdutoDetalhes',
     components: {
         NavigationDrawer,
@@ -75,7 +83,8 @@ export default {
         }
     },
     async mounted() {
-        const id = Number(this.$route.params.id);
+        const route = this.$route as Route;
+        const id = Number(route.params.id);
         try {
             const resposta = await buscarPorId(id);
             this.produto = resposta;
@@ -84,22 +93,55 @@ export default {
         } catch (e) {
             console.error('Erro ao carregar produto:', e);
         }
-
     },
-
-}
+})
 </script>
 
 
-
 <style scoped>
+.text-right {
+    text-align: right;
+}
+
+.tela-produto-detalhes {
+    background-color: #f5f5f5;
+    min-height: 100vh;
+    padding-top: 32px;
+    padding: 30px;
+}
+
+.header-linha {
+    margin-bottom: 15px;
+}
+
 .produto-detalhes {
-    padding: 20px;
+    background-color: #ffffff;
+    padding: 32px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.493);
 }
 
 .produto-detalhes h1,
-p {
+.produto-detalhes p {
     margin-bottom: 16px;
     text-align: left;
+}
+
+.v-container {
+    padding: 0 !important;
+}
+
+.bt-voltar {
+    color: rgb(24, 119, 207);
+}
+
+.titulo-pagina {
+    font-weight: bold;
+    text-align: left;
+}
+
+::v-deep main.content {
+    padding: 0 !important;
+    background-color: #f5f5f5 !important;
 }
 </style>
